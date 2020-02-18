30ba12b612c404739d6d35f302b3fbde24e20f01
Petr Fursov <p89166499800@gmail.com>
2020-02-18 20:49:33 +0300

    Домашнее задание2

--- DZ3.go
+++ DZ3.go
@@ -9,9 +9,15 @@ func main() {
 	a := big.NewInt(1)
 	b := big.NewInt(2)
 	
-	for i := 1; i < 100; i++ {
+	for i := 0; i < 100; i++{ 
+		if i == 0 {
+			fmt.Println("0")
+		} else	if i != 0 && i <= 2 {
+			fmt.Println("1")
+	} else {
 		a = a.Add(a, b)
 		a, b = b, a
 		fmt.Println(a)
+	}
 	}
 }
